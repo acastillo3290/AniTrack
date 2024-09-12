cabe49f170570b000ec576350ff6f30a8db8b8e6
Aaron Castillo <a.castillo3290@gmail.com>
2024-09-12 16:42:35 -0400

    tested mangadex api - i like

--- webapp/frontend/src/App.tsx
+++ webapp/frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  const getManga = async (title: string) => {
+    const baseUrl = "https://api.mangadex.org";
+
+    const resp = await axios({
+      method: "GET",
+      url: `${baseUrl}/manga`,
+      params: { title },
+    });
+
+    console.log(resp.data.data);
+  };
+
+  return (
+    <>
+      <button onClick={() => getManga("Naruto")}>get manga</button>
+    </>
+  );
 }
 
 export default App;
--- webapp/server/index.ts
+++ webapp/server/index.ts
@@ -6,10 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello DUDE!");
 });
 
-app.get("/hi", (req, res) => {
-  res.send("HIIIII!!!!!");
-});
-
 const port = 8080;
 app.listen(port, () => {
   console.log(`now listening on port:${port}`);
